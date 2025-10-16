a404f5a3aad59119660d929c7245961359173319
Parkar-Arslan <arslanparkar2002@gmail.com>
2025-10-16 17:46:45 -0400

    v7 minimal server

--- minimal-server.js
+++ minimal-server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/ping', (req, res) => {
   console.log('PING received!');
   res.send('PONG');
 });
 
-app.listen(5000, () => {
-  console.log('Minimal server running on http://localhost:5000');
-});
+// Export the app for Vercel
+module.exports = app;
